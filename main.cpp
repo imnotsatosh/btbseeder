2920172179f25000917bd02e3ba9de8d223f9b9f
imnotsatosh <imnotsatosh@proton.me>
2024-01-18 18:34:02 +0800

    test1

--- main.cpp
+++ main.cpp
@@ -422,7 +422,7 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
+static const string mainnet_seeds[] = { ""};
 static const string testnet_seeds[] = {"testnet-seed.alexykot.me",
                                        "testnet-seed.bitcoin.petertodd.org",
                                        "testnet-seed.bluematt.me",
