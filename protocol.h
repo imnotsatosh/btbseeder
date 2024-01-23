cc8a1044e89ccfac6f6c7d26d97c1642621f09e6
imnotsatosh <imnotsatosh@proton.me>
2024-01-23 14:20:36 +0800

    modify peer port

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 18333 : 7025;
+    return testnet ? 18333 : 3033;
 }
 
 //
